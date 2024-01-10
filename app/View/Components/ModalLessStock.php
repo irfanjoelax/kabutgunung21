4a0f7b9200a14d0d11865b48285753a0c0fbc58a
Muhammad Irfan Permana <44429347+irfanjoelax@users.noreply.github.com>
2024-01-10 11:25:23 +0800

    update jumlah minimal stok dan perubahan sku

--- app/View/Components/ModalLessStock.php
+++ app/View/Components/ModalLessStock.php
@@ -14,34 +14,9 @@ class ModalLessStock extends Component
 
     public function __construct()
     {
-        // $this->data = Penjualan::whereMonth('penjualans.created_at', date('n'))
-        //     ->whereHas('penjualan_details', function ($query) {
-        //         $query->whereHas('produk', function ($query) {
-        //             $query->where('stok', '<=', 3)->groupBy('id');
-        //         });
-        //     })->with(['penjualan_details' => function ($query) {
-        //         $query->whereHas('produk', function ($query) {
-        //             $query->where('stok', '<=', 3);
-        //         })->with('produk');
-        //     }])
-        //     ->orderBy(DB::raw('DATE(penjualans.created_at)'), 'DESC')
-        //     ->get()
-        //     // ->groupBy(DB::raw('DATE(penjualans.created_at)'))
-        //     ->groupBy(function ($item) {
-        //         return $item->created_at->format('Y-m-d');
-        //     });
-
-        // $this->data = Notifikasi::whereMonth('notifikasis.tanggal', date('n'))
-        //     ->groupBy(DB::raw('DATE(notifikasis.tanggal)'))
-        //     ->with(['produk' => function ($query) {
-        //         $query->where('stok', '<=', 3);
-        //     }])
-        //     ->orderBy(DB::raw('DATE(notifikasis.tanggal)'), 'DESC')
-        //     ->get();
-
         $this->data = Notifikasi::whereMonth('tanggal', date('n'))
             ->with(['produk' => function ($query) {
-                $query->where('stok', '<=', 3);
+                $query->where('stok', '<=', 10);
             }])
             ->orderBy('tanggal', 'DESC')
             ->get()
@@ -60,19 +35,5 @@ class ModalLessStock extends Component
         return view('components.modal-less-stock', [
             'data' => $this->data
         ]);
-
-        // dd($this->data);
-        // foreach ($this->data as $item => $notifikasi) {
-        //     echo $item . '<br>';
-        //     echo '<ul>';
-
-        //     foreach ($notifikasi as $detail) {
-        //         foreach ($detail->penjualan_details as $produk) {
-        //             echo '<li>' . $produk->produk->nama . ' - ' . $produk->produk->stok . '</li>';
-        //         }
-        //     }
-
-        //     echo '</ul>';
-        // }
     }
 }
